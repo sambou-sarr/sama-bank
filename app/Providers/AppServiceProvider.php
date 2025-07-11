c37c4ff0b802cbb3fa1cc26441291edfb0494cd3
sambou <sarrsambou03@gmail.com>
2025-07-11 15:47:33 +0000

    protole

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         Paginator::useBootstrap(); 
 
-     
+        URL::forceScheme('https');
     }
 
 
